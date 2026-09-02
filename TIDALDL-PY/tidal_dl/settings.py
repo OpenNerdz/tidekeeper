3fa121eb4a79b135d9dd921fb2201b21d6da0667
OpenNerdz <gta.endor@gmail.com>
2026-09-02 16:39:52 +0100

    Finish leftover GUI settings, path, search, and 429 fixes

--- TIDALDL-PY/tidal_dl/settings.py
+++ TIDALDL-PY/tidal_dl/settings.py
@@ -73,7 +73,7 @@ class Settings(aigpy.model.ModelBase):
     downloadDelay = True
     requestIntervalSeconds = 3.0
     adaptiveRateLimit = True
-    saveAsFlac = False
+    saveAsFlac = True
 
     downloadPath = "./download/"
     audioQuality = AudioQuality.Max
